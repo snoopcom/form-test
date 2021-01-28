4b0e4621b1b80bbdbb9c8cf098e25f0e04cbe2fd
Alex <snoopcom@bk.ru>
2021-01-28 14:14:40 +0300

    Updates

---
+++ precache-manifest.b460ff6c887dc207d2706f6a2cdaea08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060ccff657028140536ac51de70e9d0d",
+    "url": "/form-test/index.html"
+  },
+  {
+    "revision": "2af2df99d7df0b55b61c",
+    "url": "/form-test/static/css/2.0d383db3.chunk.css"
+  },
+  {
+    "revision": "137de2b138ffd2252dce",
+    "url": "/form-test/static/css/main.3089b04a.chunk.css"
+  },
+  {
+    "revision": "2af2df99d7df0b55b61c",
+    "url": "/form-test/static/js/2.9656a8b6.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/form-test/static/js/2.9656a8b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "137de2b138ffd2252dce",
+    "url": "/form-test/static/js/main.2b65253c.chunk.js"
+  },
+  {
+    "revision": "eec33bf65d57c6e57cac",
+    "url": "/form-test/static/js/runtime-main.b811a38a.js"
+  }
+]);
